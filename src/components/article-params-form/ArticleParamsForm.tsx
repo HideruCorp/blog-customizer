534fb4745eeca50c6fa6bbcd22a50a864b5d3aa7
Aziev Damir <Hider998@gmail.com>
2025-10-01 21:27:29 +0500

    feat(sidebar): sidebar opening
    - ArticleParamsForm.tsx - добавлены пропсы isFormOpen и onToggle для управления видимостью формы
    - index.tsx - добавлен state для управления articleState и isFormOpen с обработчиком handleFormToggle

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -2,12 +2,22 @@ import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
 
 import styles from './ArticleParamsForm.module.scss';
+import clsx from 'clsx';
 
-export const ArticleParamsForm = () => {
+type ArticleParamsFormProps = {
+	isFormOpen: boolean;
+	onToggle: () => void;
+};
+
+export const ArticleParamsForm = ({
+	isFormOpen,
+	onToggle,
+}: ArticleParamsFormProps) => {
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
+			<ArrowButton isOpen={isFormOpen} onClick={onToggle} />
+			<aside
+				className={clsx(styles.container, isFormOpen && styles.container_open)}>
 				<form className={styles.form}>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
