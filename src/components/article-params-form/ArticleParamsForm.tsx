961ac638937f2af049545e04dd41b7f0e9809913
Aziev Damir <Hider998@gmail.com>
2025-10-02 19:54:07 +0500

    feat(article-params-form): customization settings added
    - ArticleParamsForm.tsx - добавлены Select и RadioGroup компоненты для выбора шрифта, размера, цветов и ширины контента

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,13 +1,22 @@
-import { ArrowButton } from 'src/ui/arrow-button';
-import { Button } from 'src/ui/button';
 import {
-	defaultArticleState,
 	ArticleStateType,
+	backgroundColors,
+	contentWidthArr,
+	defaultArticleState,
+	fontColors,
+	fontFamilyOptions,
+	fontSizeOptions,
 } from 'src/constants/articleProps';
 
-import styles from './ArticleParamsForm.module.scss';
-import clsx from 'clsx';
+import { ArrowButton } from 'src/ui/arrow-button';
+import { Button } from 'src/ui/button';
+import { RadioGroup } from 'src/ui/radio-group';
+import { Select } from 'src/ui/select';
+import { Separator } from 'src/ui/separator';
+
 import { FormEvent, useState } from 'react';
+import clsx from 'clsx';
+import styles from './ArticleParamsForm.module.scss';
 
 type ArticleParamsFormProps = {
 	isFormOpen: boolean;
@@ -43,6 +52,56 @@ export const ArticleParamsForm = ({
 					className={styles.form}
 					onSubmit={handleSubmit}
 					onReset={handleReset}>
+					<Select
+						title='Шрифт'
+						options={fontFamilyOptions}
+						selected={formState.fontFamilyOption}
+						onChange={(option) =>
+							setFormState((prev) => ({ ...prev, fontFamilyOption: option }))
+						}
+					/>
+					<Separator />
+
+					<RadioGroup
+						name='fontSize'
+						title='Размер шрифта'
+						options={fontSizeOptions}
+						selected={formState.fontSizeOption}
+						onChange={(option) =>
+							setFormState((prev) => ({ ...prev, fontSizeOption: option }))
+						}
+					/>
+					<Separator />
+
+					<Select
+						title='Цвет шрифта'
+						options={fontColors}
+						selected={formState.fontColor}
+						onChange={(option) =>
+							setFormState((prev) => ({ ...prev, fontColor: option }))
+						}
+					/>
+					<Separator />
+
+					<Select
+						title='Цвет фона'
+						options={backgroundColors}
+						selected={formState.backgroundColor}
+						onChange={(option) =>
+							setFormState((prev) => ({ ...prev, backgroundColor: option }))
+						}
+					/>
+					<Separator />
+
+					<RadioGroup
+						name='contentWidth'
+						title='Ширина контента'
+						options={contentWidthArr}
+						selected={formState.contentWidth}
+						onChange={(option) =>
+							setFormState((prev) => ({ ...prev, contentWidth: option }))
+						}
+					/>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
 						<Button title='Применить' htmlType='submit' type='apply' />
